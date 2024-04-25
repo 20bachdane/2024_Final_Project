62900f0a499e1c430108edb53917702230d6c807
RyanMroczenski <157651803+RyanMroczenski@users.noreply.github.com>
2024-04-25 14:39:35 -0500

    Changes

--- js/main.js
+++ js/main.js
@@ -7,21 +7,28 @@ document.addEventListener('DOMContentLoaded', createMap);
 function createMap() {
     // Create the map
     map = L.map('map', {
-        center: [0, 0],
-        zoom: 2,
+        center: [37.0902, -95.7129],  // Centered on the U.S.
+        zoom: 4,  
         minZoom: 2
     });
 
      // Define tile layers
     var stamenLayer = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_terrain/{z}/{x}/{y}{r}.png', {
         attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>',
-        
+
     });
      var osmLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a> | Data source: <a href="https://data.worldbank.org/indicator/GC.DOD.TOTL.GD.ZS">World Bank</a>'
     });
+    var worldLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    });
+    var Stadia_AlidadeSatellite = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}', {
 
- 
+        attribution: '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        ext: 'jpg'
+    });
+
 
     // Add default tile layer (OSM)
     stamenLayer.addTo(map);
@@ -29,33 +36,13 @@ function createMap() {
     // Add layer control to toggle between layers
     var baseLayers = {
         "Stamen Terrain": stamenLayer,
-        "OpenStreetMap": osmLayer
+        "OpenStreetMap": osmLayer,
+        "World Imagery": worldLayer,
+        "Satellite": Stadia_AlidadeSatellite,
         
     };
 
     L.control.layers(baseLayers).addTo(map);
-
-
-    // Add title control
-    var TitleControl = L.Control.extend({
-        options: {
-            position: 'topleft' // Adjusted position to 'topleft' for pixel positioning
-        },
-    
-        onAdd: function () {
-            var container = L.DomUtil.create('div', 'map-title');
-            
-            // Set pixel values for positioning
-            container.style.top = '-75px'; // Adjust as needed
-            container.style.left = '570px'; // Adjust as needed
-            container.style.backgroundColor = 'beige'; // Set background color to white
-    
-            container.innerHTML = '<h1>National Debt Map 2000 - 2020</h1>'; // Change the title as needed
-            return container;
-        }
-    });
-    
-    map.addControl(new TitleControl());
 }    
     // Function to handle parsing CSV data
     function parseCSVData(csvData) {
